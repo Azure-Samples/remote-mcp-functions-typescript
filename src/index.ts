4834fb95786cbecbeab28bd2771c92d138dbc6ba
Swapnil Nagar <swapnilnagar@microsoft.com>
2026-02-04 15:58:44 -0800

    Adding the WeatherMcpApp

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ import { app } from '@azure/functions';
 app.setup({
     enableHttpStream: true,
 });
+
+// Import functions to register them
+import './functions/helloMcpTool';
+import './functions/snippetsMcpTool';
+import './functions/weatherMcpApp';
